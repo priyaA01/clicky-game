9448fdee96c4507445cfffd1a31e547de9627d68
priyaA01 <pryshanthi.it@gmail.com>
2018-03-24 13:58:02 -0500

    updated score on click

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     friends : friends,
-    msg: "Click an image to begin!",
+    msg: "Click image once to score!",
     topscore: 0,
     score: 0,
     unselected:friends
@@ -19,21 +19,19 @@ class App extends Component {
     const findImage = this.state.unselected.find(item => item.image === image);
 
     if(findImage === undefined) {
-        // failure to select a new dog
         this.setState({ 
-            msg: "You guessed incorrectly!",
-            topscore: (this.state.score > this.state.topscore) ? this.state.score : this.state.topsscore,
+            msg: "You clicked image twice!",
+            topscore: (this.state.score > this.state.topscore) ? this.state.score : this.state.topscore,
             score: 0,
             friends: friends,
             unselected: friends
         });
     }
     else {
-        // success to select a new dog
         const newImages = this.state.unselected.filter(item => item.image !== image);
         
         this.setState({ 
-            msg: "You guessed correctly!",
+            msg: "You clicked image, Score!",
             score: this.state.score + 1,
             friends: friends,
             unselected: newImages
@@ -41,13 +39,13 @@ class App extends Component {
     }
   };
 
-  /*moveImage = id => {
+  moveImage = image => {
     // Filter this.state.friends for friends with an id not equal to the id being removed
     const friends = this.state.friends.filter(friend => friend.id !== id);
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
   };
-*/
+
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
@@ -62,6 +60,7 @@ class App extends Component {
         {this.state.friends.map(friend => (
           <FriendCard
             scoreChange={this.scoreChange}
+            moveImage={this.moveImage}
             id={friend.id}
             key={friend.id}
             name={friend.name}
